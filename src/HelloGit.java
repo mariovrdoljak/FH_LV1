933ef3caa0bce3acfc44e0d74e1c9c652f739b9d
Mario Vrdoljak <mario.vrdoljak@edu.campus02.at>
2015-10-10 15:14:28 +0200

    main lv1

---
+++ src/HelloGit.java
@@ -0,0 +1,26 @@
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallo in der LV1");
+
+		boolean treeFront = true;
+		int numberLeaves = 0;
+		
+		char meinZeichen = 'x';
+
+		// println(); + "\n"
+		System.out.println("Ist Baum voraus? " + treeFront);
+		System.out.println("Anzahl Blätter: " + numberLeaves);
+		System.out.println(meinZeichen);
+
+		/*
+		 * mit (String[] args) können Parameter übergeben werden
+		 * 
+		 * 
+		 */
+
+	}
+
+}
